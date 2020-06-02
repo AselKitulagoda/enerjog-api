2dc9a80eda94a4476bfd882e7230bbfe02bb383e
asel <asel.kitulagoda@gmail.com>
2020-06-02 20:55:59 +0100

    added route

--- start/routes.js
+++ start/routes.js
@@ -5,5 +5,6 @@ const Route = use('Route')
 Route.group(() => {
   Route.post('login', 'Auth/AuthenticationController.login')
   Route.post('register', 'Auth/AuthenticationController.register')
+  Route.post('change', 'Auth/AuthenticationController.change')
   Route.get('me', 'Auth/AuthenticationController.me').middleware(['auth'])
 }).prefix('api')
